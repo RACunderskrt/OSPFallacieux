7a24cdb33029d783ec7b7052f5fdfb335199a21b
RACunderskrt <manuel.humbert42@gmail.com>
2025-06-10 09:56:33 +0200

    new test added

--- OSPFallacieux/main.cpp
+++ OSPFallacieux/main.cpp
@@ -27,7 +27,21 @@ void test3(){
     cout << "topo apres : \n" << topo.size() << endl;
 }
 
+void test5(){
+    Router r4 = Router("R4","/etc/ospfallacieux/config");
+    Topology topo2;
+    topo2.add(r4);
+    
+    Topology topo;
+    topo.init_test();
+    cout << "topo avant : \n" << topo << endl;
+    topo2.normalize();
+    topo.from_serialized(topo2.getRoutersSerialized(), topo2.getReseauxSerialized());
+    cout << "==============================" << endl;
+    cout << "topo apres : \n" << topo << endl;
+}
+
 int main(){
-    test3();
+    test5();
     return 0;
 }
